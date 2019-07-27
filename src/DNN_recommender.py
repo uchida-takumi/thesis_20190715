18e24b8b2193602c14274eb41fa18cc51359b384
takumi uchida <takumi.uchida.research@gmail.com>
2019-07-27 15:59:58 +0900

    wip

--- src/DNN_recommender.py
+++ src/DNN_recommender.py
@@ -46,8 +46,54 @@ def RFNN(max_user, max_item,
     return model
     
 
-def R_Wide_and_Deep
+def R_Wide_and_Deep(max_user, max_item, 
+         embedding_size=8, dnn_hidden_units=(128, 128), l2_reg=1e-5):
+    
+    # --- INPUT --- #
+    input_user = Input(shape=(1,), name='user')
+    input_item = Input(shape=(1,), name='item')
+    
+    # --- WIDE part --- #
+    ## binary embedding
+    user_binary_embedding = id_binary_embedding(
+            input_user, max_user, l2_reg, sufix_name='wide_user')
+    item_binary_embedding = id_binary_embedding(
+            input_item, max_item, l2_reg, sufix_name='wide_item')    
+    wide_x = Concatenate(name='wide_user_item_concatenate'
+                         )([user_binary_embedding, item_binary_embedding])
+    
+    # --- DEEP part --- #
+    ## embbedding
+    user_embedding = id_embedding(
+            input_user, max_user, embedding_size, l2_reg, sufix_name='deep_user')
+    item_embedding = id_embedding(
+            input_item, max_item, embedding_size, l2_reg, sufix_name='deep_item')    
+    deep_x = Concatenate(name='deep_user_item_concatenate'
+                    )([user_embedding, item_embedding])
+    
+    deep_x = multiple_hidden(
+            deep_x, dnn_hidden_units, l2_reg, prefix_name='deep_hidden')
+    
+    # --- OUTPUT --- #
+    x = Concatenate(name='wide_deep_concatenate'
+                    )([wide_x, deep_x])
+    output = Dense(
+                    1, activation=None, use_bias=True,
+                    kernel_regularizer=regularizers.l2(l2_reg),
+                    name='output'
+                   )(x)
 
+    # --- build as MODEL --- #
+    model = Model(inputs=[input_user, input_item], outputs=[output])
+    
+    ## compile 
+    model.compile(
+            optimizer=optimizers.Adam(),
+            loss=losses.mean_absolute_error
+            )    
+    
+    return model
+    
 
 #####################
 # sub module
@@ -62,6 +108,9 @@ def id_embedding(input_, max_, embedding_size, l2_reg, sufix_name):
     id_embedding = Flatten(name='flatten_{}'.format(sufix_name))(id_embedding)
     return id_embedding
 
+def id_binary_embedding(input_, max_, l2_reg, sufix_name):
+    return id_embedding(input_, max_, 1, l2_reg, sufix_name)
+
 def multiple_hidden(x, dnn_hidden_units, l2_reg, prefix_name):
     for i,d in enumerate(dnn_hidden_units):
         x = Dense(
@@ -70,10 +119,12 @@ def multiple_hidden(x, dnn_hidden_units, l2_reg, prefix_name):
                 name=prefix_name+'_{}'.format(i)
                 )(x)
     return x
-    
+
     
 #####################
 if __name__ == 'how to use':
+    
+    # load dataset
     import numpy as np
     X = np.array([
             [1,2],
@@ -86,10 +137,10 @@ if __name__ == 'how to use':
             4,
             1,
             3,
-            ])
-    
+            ])    
     max_user, max_item = X[:,0].max()+1, X[:,1].max()+1
     
+    # --- RFNN --- 
     model = RFNN(max_user, max_item)    
     model.fit(
             x={'user':X[:,0], 'item':X[:,1]}, 
@@ -102,6 +153,18 @@ if __name__ == 'how to use':
     # predcit on new ids
     model.predict(x={'user':np.array([0,0,1]), 'item':np.array([0,1,0])})
         
+    # --- R_Wide_and_Deep --- 
+    model = R_Wide_and_Deep(max_user, max_item)    
+    model.fit(
+            x={'user':X[:,0], 'item':X[:,1]}, 
+            y={'output':y},
+            batch_size=1, epochs=20)
+    
+    # predict on in-samples
+    model.predict(x={'user':X[:,0], 'item':X[:,1]})
+    
+    # predcit on new ids
+    model.predict(x={'user':np.array([0,0,1]), 'item':np.array([0,1,0])})
     
     
     
