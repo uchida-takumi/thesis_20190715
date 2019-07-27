39d62177dab67eb1fbad2a331f89e5afc366b073
takumi uchida <takumi.uchida.research@gmail.com>
2019-07-27 15:27:03 +0900

    wip

--- src/DNN_recommender.py
+++ src/DNN_recommender.py
@@ -11,11 +11,11 @@ from tensorflow.keras.models import Model
 
 almost0init = initializers.RandomUniform(minval=-1e-5, maxval=1e-5)
 
-def RFNN(self, max_user, max_item, 
+def RFNN(max_user, max_item, 
          embedding_size=8, dnn_hidden_units=(128, 128), l2_reg=1e-5):    
     # --- INPUT --- #
-    input_user = Input(shape=(1,), name='input_user')
-    input_item = Input(shape=(1,), name='input_item')
+    input_user = Input(shape=(1,), name='user')
+    input_item = Input(shape=(1,), name='item')
 
     # --- Embedding --- #
     user_embedding = id_embedding(
@@ -82,5 +82,40 @@ def id_embedding(input_, max_, embedding_size, l2_reg, sufix_name):
     return id_embedding
 
     
+#####################
+if __name__ == 'how to use':
+    import numpy as np
+    X = np.array([
+            [1,2],
+            [1,3],
+            [2,1],
+            [2,2],
+            ])     
+    y = np.array([
+            5,
+            4,
+            1,
+            3,
+            ])
     
+    max_user, max_item = X[:,0].max()+1, X[:,1].max()+1
+    
+    model = RFNN(max_user, max_item)    
+    model.fit(
+            x={'user':X[:,0], 'item':X[:,1]}, 
+            y={'output':y},
+            batch_size=1, epochs=20)
+    
+    # predict on in-samples
+    model.predict(x={'user':X[:,0], 'item':X[:,1]})
+    
+    # predcit on new ids
+    model.predict(x={'user':np.array([0,0,1]), 'item':np.array([0,1,0])})
+        
+    
+    
+    
+    
+    
+        
     
